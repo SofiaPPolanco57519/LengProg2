6894340c1b7cafdea53f00c91caf1dbce7875f4b
SofiaPPolanco57519 <132014940+SofiaPPolanco57519@users.noreply.github.com>
2023-10-06 09:10:38 -0600

    Add files via upload

--- Pokemon_Herencia/BatallaPokemon.java
+++ Pokemon_Herencia/BatallaPokemon.java
@@ -3,15 +3,12 @@ package pokemon_herencia;
 
 
 public class BatallaPokemon {
-    
-    
-    
 
-public static void main(String[] args) {
-    Pokemon Mimikyu= new Pokemon ("Mimikyu", "Fantasma", 10);
-    Pokemon Jigglypuff= new Pokemon ("Jigglypuff", "Hada", 10);
-    Mimikyu.atacar("Bola sombra", Jigglypuff);
-    Jigglypuff.atacar("Carantoña", Mimikyu);
+    public static void main(String[] args) {
+        Pokemon Mimikyu= new Pokemon ("Mimikyu", "Fantasma", 10);
+        Pokemon Jigglypuff= new Pokemon ("Jigglypuff", "Hada", 10);
+        Mimikyu.atacar("Bola sombra", Jigglypuff);
+        Jigglypuff.atacar("Carantoña", Mimikyu);
 
     }
 }
--- Pokemon_Herencia/Pokemon.java
+++ Pokemon_Herencia/Pokemon.java
@@ -3,14 +3,13 @@ package pokemon_herencia;
 
 
 public class Pokemon {
-    private int hp, nivel;
     private String nombre, tipo;
+    private int hp =100;
+    private int nivel;
+    
 
-    public Pokemon() {
-    }
-
+  
     public Pokemon(String nombre, String tipo, int nivel) {
-        int hp = 100;
         this.nivel = nivel;
         this.nombre = nombre;
         this.tipo = tipo;
@@ -34,18 +33,18 @@ public class Pokemon {
     
     private void calculaDanio(int danio){
         this.hp -= danio;
-        System.out.printf("%s recibe %d puntos de danio\n",
-                this.getNombre(), danio);
+        System.out.printf("%s recibe %d puntos de danio\n", this.getNombre(), danio);
     }
     
     public void recibirAtaque(String movimiento){
-        System.out.printf("%s recibe ATK %s\n", this.getNombre(),
-                movimiento);
-        calculaDanio((int) Math.random() * 10 + 1);
+        System.out.printf("%s recibe ATK %s\n", this.getNombre(), movimiento);
+        double numero = Math.random();
+        calculaDanio((int) (numero * 10 + 1));
+        System.out.printf("%s tiene ahora %s puntos de vida\n", this.getNombre(),this.hp);
     }
     
     public void atacar(String movimiento, Pokemon pokemon){
-        System.out.printf("%s ataca a %s con %s\n", this.getNombre(),
+        System.out.printf("\n%s ataca a %s con %s\n", this.getNombre(),
                 pokemon.getNombre(), movimiento);
                 pokemon.recibirAtaque(movimiento);
     }
